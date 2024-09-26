b35d9447305cb6bb8f9960ce9744ca837943f476
Arjun11012 <131374194+Arjun11012@users.noreply.github.com>
2024-09-26 05:00:14 +0000

    changes to oeration

--- 01_basics/operation.js
+++ 01_basics/operation.js
@@ -1,6 +1,44 @@
 // **** Operations ****
-// node 01_basics/03_conversionOperation.js
+// node 01_basics/operation.js
 
 let value = 3
 let negValue = -value
 console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);  // to duplicate a line of code in vscode shortcut is shift + alt + downkey
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2); // to open terminal shortcut is clt + shift + C
+
+
+let str1 = "hello"
+let str2 = " arjun"
+
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + 2 + 2);
+// console.log(1 + 2 + "2"); // this are some bad practice to write code
+
+// console.log( (3 + 4) * 5 % 3); // ecma scritp 7.1 ToPrimitive
+
+// console.log(true);
+// console.log(true+); // not good code
+// console.log(+""); // not good code
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;  // Prefix and Postfix operators // gameCounter++
+//prefix and postfix js mdn
+console.log(gameCounter);
+
+
+//link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
