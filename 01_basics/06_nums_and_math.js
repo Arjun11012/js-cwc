9d5bec4f118c5f086150480d716add1745da7335
Arjun11012 <131374194+Arjun11012@users.noreply.github.com>
2024-09-26 11:06:52 +0000

    nums and math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,19 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100) //explicitly defining // new object which is a number type
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2)); // precision value is very big in ecommerce website then we use this method
+
+const otherNumber = 123.8966 //23.8966
+
+console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+
